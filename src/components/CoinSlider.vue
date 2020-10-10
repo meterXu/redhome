9d6873ad6442f1d74bcb945d3b63e1f1ce8f14a1
xuhg <xuhg@dpark.com.cn>
2020-10-10 15:19:40 +0800

    ...

---
+++ src/components/CoinSlider.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="bg">
+
+  </div>
+</template>
+
+<script>
+export default {
+    name:"CoinSlider",
+    props:{
+        width:{
+            default:565,
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Wallpaper.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Wallpaper',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
