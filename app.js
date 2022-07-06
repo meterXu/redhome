d8ecb97808e92e8c94cb2441fc12e363682a81dd
isaac <xuhonggang@hotmail.com>
2022-07-06 11:18:39 +0800

    ...

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.urlencoded({extended: true,limit: '50mb'}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
